039705d1d78f497f7e3e6d82cd7e0401a615d65b
Mert Samet Kayacioglu <mskayacioglu@icloud.com>
2025-06-19 17:51:08 +0300

    clang-format -i InputAndOutput.cpp

--- InputAndOutput.cpp
+++ InputAndOutput.cpp
@@ -1,16 +1,15 @@
+#include <algorithm>
 #include <cmath>
 #include <cstdio>
-#include <vector>
 #include <iostream>
-#include <algorithm>
+#include <vector>
 using namespace std;
 
-
 int main() {
-    int a, b, c;
-    
-    cin >> a >> b >> c;
-    cout << (a + b + c) << endl;
-    
-    return 0;
+  int a, b, c;
+
+  cin >> a >> b >> c;
+  cout << (a + b + c) << endl;
+
+  return 0;
 }
