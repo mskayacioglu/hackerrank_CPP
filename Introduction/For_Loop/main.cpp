dc751523508566eb61ba0f136bb92ce38856ed43
Mert Samet Kayacioglu <mskayacioglu@icloud.com>
2025-06-21 15:33:08 +0300

    For Loop

---
+++ Introduction/For_Loop/main.cpp
@@ -0,0 +1,42 @@
+#include <map>
+#include <set>
+#include <list>
+#include <cmath>
+#include <ctime>
+#include <deque>
+#include <queue>
+#include <stack>
+#include <string>
+#include <bitset>
+#include <cstdio>
+#include <limits>
+#include <vector>
+#include <climits>
+#include <cstring>
+#include <cstdlib>
+#include <fstream>
+#include <numeric>
+#include <sstream>
+#include <iostream>
+#include <algorithm>
+#include <unordered_map>
+
+using namespace std;
+int main() {
+    int a, b;
+    cin >> a >> b;
+
+    string numbers[] = {
+        "", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine"
+    };
+
+    for (int i = a; i <= b; i++) {
+        if (i >= 1 && i <= 9) {
+            cout << numbers[i] << endl;
+        } else {
+            cout << (i % 2 == 0 ? "even" : "odd") << endl;
+        }
+    }
+
+    return 0;
+}
--- README.md
+++ README.md
@@ -9,3 +9,4 @@
 1. [Say "Hello, World!" With C++](https://www.hackerrank.com/challenges/cpp-hello-world/problem?isFullScreen=true)  
 2. [Input and Output](https://www.hackerrank.com/challenges/cpp-input-and-output/problem?isFullScreen=true)  
 3. [Basic Data Types](https://www.hackerrank.com/challenges/c-tutorial-basic-data-types/problem?isFullScreen=true)
+4. [For Loop](https://www.hackerrank.com/challenges/c-tutorial-for-loop/problem?isFullScreen=true)
